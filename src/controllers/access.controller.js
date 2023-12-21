8ebc70d8d5ba6f74f26b8b95077b196b6f50a4a9
Cristian3410st <cristian8090tokost@gmail.com>
2023-12-21 17:00:48 -0500

    validacion1

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -81,7 +81,7 @@ export const login = async (req, res) => {
               };
 
               const token = await createAccessToken({ id: saveUser.id });
-              Cookies.set("token",token)
+              Cookies.set("token", token, { secure: true });
               console.log("token creado",token)
               console.log('Token almacenado en cookies.');
               res.status(200).json({
